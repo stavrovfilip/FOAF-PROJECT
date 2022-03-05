d004ad93377bae0b6f7b9a604813cf8f036c6358
unknown <stavrovf@gmail.com>
2022-03-05 12:35:49 +0100

    Project creation

---
+++ src/main/java/mk/ukim/finki/foafprofile/FoafProfileApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.foafprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoafProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoafProfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/foafprofile/FoafProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.foafprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoafProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
