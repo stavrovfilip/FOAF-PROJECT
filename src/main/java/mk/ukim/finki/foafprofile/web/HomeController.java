2e25006bb7e0359bbffae376d5a30659c505735a
unknown <stavrovf@gmail.com>
2022-03-26 22:42:07 +0100

    Header, footer, homepage, and login added

---
+++ src/main/java/mk/ukim/finki/foafprofile/web/HomeController.java
@@ -0,0 +1,19 @@
+package mk.ukim.finki.foafprofile.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public String getHomePage(Model model, HttpServletRequest request) {
+        model.addAttribute("bodyContent", "home");
+        return "master-template";
+    }
+}
