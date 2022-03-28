8c7fa1e6d6665f7126a6b228fd1fde556af238ea
StojanovskaS <sandrastojanovska99@yahoo.com>
2022-03-28 20:50:36 +0200

    additional design added

---
+++ src/main/java/mk/ukim/finki/foafprofile/web/ImageController.java
@@ -0,0 +1,28 @@
+package mk.ukim.finki.foafprofile.web;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Controller
+public class ImageController {
+    @GetMapping(value = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<Resource> image() throws IOException {
+        final ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(Paths.get(
+                "C:\\Users\\Sandra\\IdeaProjects\\FOAF-PROJECT\\src\\main\\resources\\static\\images\\slika60.jpg"
+        )));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentLength(inputStream.contentLength())
+                .body(inputStream);
+
+    }
+}
